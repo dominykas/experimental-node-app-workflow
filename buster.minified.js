efb844f2370ac1da0dc1211530d06104f8291ebd
Dominykas Blyžė <hello@dominykas.com>
2012-07-14 21:26:13 +0300

    patched up buster-amd to enable "logical" config for minified tests...

--- buster.minified.js
+++ buster.minified.js
@@ -4,19 +4,17 @@ config["Browser tests"] = {
 	rootPath:".",
 	environment:"browser",
 	sources:[
-		// note: buster-amd ignores the below line and expects RequireJS to pre-load things
 		"min/all.js"
 	],
 	tests:[
-		// note: buster-amd only uses the module names and expects RequireJS to pre-load things
 		"test/**-common.test.js",
 		"test/**-browser.test.js"
 	],
 	libs:[
-		// note: must have this pre-loaded because almond.js does not include a script loader
-		"min/all.js",
-		"test/**-common.test.js",
-		"test/**-browser.test.js"
 	],
-	extensions:[require("buster-amd")]
+	extensions:[require("buster-amd")],
+	"buster-amd": {
+		preloadTests: true,
+		preloadSources: true
+	}
 };
